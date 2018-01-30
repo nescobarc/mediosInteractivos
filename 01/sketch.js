450ae5ba7a902d352701e4685e70c87eb59edae3
nescobarc <35973458+nescobarc@users.noreply.github.com>
2018-01-30 14:46:04 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,44 @@
+function setup() { 
+  createCanvas(800,500);
+  background(229,223,214);
+  
+  noStroke();
+  fill(219,212,128);
+  ellipse(400,270,300,300); //Amarillo
+  
+  
+  strokeWeight(1);
+  stroke(0,0,0);
+  fill(212,168,142);
+  ellipse(390,260,230,230); //Naranja
+  
+  strokeWeight(1);
+  stroke(0,0,0);
+  fill(55,95,146);
+  ellipse(380,250,180,180); //Azul
+	
+	strokeWeight(5);
+  stroke(0,0,0);
+  line(0,300,520,0); //1
+  
+  strokeWeight(4);
+  line(50,490,480,100); //2
+  
+  strokeWeight(6);
+  line(660,0,560,300); //3
+  
+  strokeWeight(5); 
+  line(460,80,780,80); //Horizontal
+  line(470,150,800,150); //Horizontal
+
+  
+  
+	
+	
+	
+	
+	
+	
+	
+	
+}
